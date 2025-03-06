e6595b8369db8fbeb5be4f3919662cd92fd6d8fc
Juan Pablo Perez Fernandez <blackstar371@outlook.com>
2025-03-05 20:34:06 -0600

    Modificacion sobre las compras

--- utl_tienda/Compras/urls.py
+++ utl_tienda/Compras/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from Compras.views import CargarProductoView, ListadoComprasView, ListadoTodasLasComprasView
+from Compras.views import ListadoComprasView, ListadoTodasLasComprasView, ProductoSeleccionadoView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('cargar_producto/<int:id>', login_required(CargarProductoView.as_view()), name='cargar_producto'),
     path('listado_compras/<int:id>', login_required(ListadoComprasView.as_view()), name='listado_compras'),
     path('lista_todas_compras/', ListadoTodasLasComprasView.as_view(), name='lista_todas_compras'),
+    path('producto_seleccionado/<int:id>', ProductoSeleccionadoView.as_view(), name='producto_seleccionado')
 ]
--- utl_tienda/Compras/views.py
+++ utl_tienda/Compras/views.py
@@ -5,6 +5,7 @@ from . import forms
 from Productos.models import Producto
 from Compras.models import Compra
 from django.contrib.auth.models import User
+from Compras.forms import CompraCrearForm
 
 class ListadoComprasView(TemplateView):
     template_name = 'historial_compras.html'
@@ -22,35 +23,20 @@ class ListadoTodasLasComprasView(TemplateView):
     def get_context_data(self):
         lista = Compra.objects.all()
         return {'lista': lista}
-
-# El kwargs es parecido al contexto de algunos metodos. En este se usa para obtener la informacion de un objeto entero y llega como parametro parecido a las funciones de POST enviando el objeto esperado
-# pero en este caso recibiendolo.
-class CargarProductoView(FormView): 
+    
+class ProductoSeleccionadoView(FormView):
     template_name = 'compra_productos.html'
     form_class = forms.CompraCrearForm
     success_url = reverse_lazy('lista_productos')
+    idProducto = 0
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         id = self.kwargs.get('id')
         producto = get_object_or_404(Producto, id=id)
-        kwargs['initial'] = { # Creamos un objeto con los atributos que llegaran del kwargs para despues retornarlo en el contexto y asi poder usar esos atributos para mostrar los detalles del producto
-            'producto': producto.id,
-            'nombre_producto': producto.producto_nombre,
-            'precio_producto': producto.producto_precio,
-            'imagen_url': producto.producto_imagen.url if producto.producto_imagen else '' # validacion simple y pa' toda la familia donde retorne la url o una cadenita vacia
-        }
+        kwargs['instance'] = producto
         return kwargs
 
     def form_valid(self, form):
-        producto = get_object_or_404(Producto, id=self.kwargs['id']) # obtenemos el producto antes mecionado desde el kwargs con la funcion perrona donde regresa el objeto o nel por el id
-        compra = Compra( # Creamos un objeto con los atributos para poder guardarlo dentro de la BADA
-            producto=producto,
-            nombre_producto=producto.producto_nombre,
-            cantidad=form.cleaned_data['cantidad'],
-            precio_producto=producto.producto_precio,
-            precio_total=form.cleaned_data['cantidad'], #producto.producto_precio, # Multiplicacion simple para mostrar el total que no pidieron en el proyecto pero me da amsiedad no mostrar (No se va a mostrar jeje)
-            comprado_por=self.request.user
-        )
-        compra.save()
+        form.save()
         return super().form_valid(form)
